46f72ab2204e61cc027a74556f46a1ea47602b71
SamaNaEz <samuelnataren6@gmail.com>
2021-06-04 14:48:52 -0500

    Deleted extra files

--- spec/cash_dispenser_spec.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../cash_dispenser'
-
-RSpec.describe CashDispenser do
-  let(:cls) { described_class.new }
-
-  it 'Test sufficient amount' do
-    expect(cls.is_sufficient_cash_available(455)).to be_truthy
-  end
-end
